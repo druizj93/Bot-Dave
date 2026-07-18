e4239d58e388035f05dfc3ce46ab5173f344e85c
druizj93 <d.ruizj93@gmail.com>
2026-07-18 23:59:07 +0200

    Delete youtube.py

--- youtube.py
+++
@@ -1,8 +0,0 @@
-import yt_dlp
-
-class YouTubeModule:
-    def download_info(self, url):
-        """Obtiene info de video YouTube."""
-        with yt_dlp.YoutubeDL() as ydl:
-            info = ydl.extract_info(url, download=False)
-            return info.get('title', 'No title')
